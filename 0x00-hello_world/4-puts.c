f95a46d2365b18977a29fade55172df254a8abde
ayomidekappo12 <ayomidekappo12@gmail.com>
2022-12-08 12:47:19 -0800

    tasks 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,8 @@
 
 /**
  * main - Entry point
+ * prints the phrase programming is like building a multilingual puzzle
  *
- * return: Always 0 (Success)
+ * Return: 0 if exited properly, non-zero otherwise
+ *
  */
-
-int main(void)
-{
-      puts("\"programming is like building a multilingual puzzle");
-      return (0);
-
-}      
