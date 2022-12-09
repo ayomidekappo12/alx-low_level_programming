28f11a00f6b78327acdd4b59ed4a97044a630bbd
ayomidekappo12 <ayomidekappo12@gmail.com>
2022-12-09 12:58:08 -0800

    tasks 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,12 +4,16 @@
 
 /**
  * main - Entry point
- * description: get a random to the variable n each time it is executed
+ *
+ * description: 'get a random to the variable n each time it is executed'
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
