598705615445b86268d8f811b07f55ce7befbdc9
ayomidekappo12 <ayomidekappo12@gmail.com>
2022-12-08 20:36:29 -0800

    task 6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,8 +5,9 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	puts("\"Programming is like building a multiligual puzzle");
+	printf("\"with proper grammer, but the outcome is a piece of art,");
 	return (0);
 }
