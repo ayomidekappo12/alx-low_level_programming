42ee892d2ff2b430157862d6bc0471a99498b204
ayomidekappo12 <ayomidekappo12@gmail.com>
2022-12-13 22:14:01 -0800

    tasks 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,21 +1,25 @@
 #include "main.h"
 
 /**
- * main - check the code.
+ * test_islower - Test the _islower function
  *
- * Return: Always 0.
+ * @n: Number to pass to _islower function
  */
-
-int main(void)
+void test_islower(int n)
 {
 	int r;
 
-	r = _islower('H');
-	_putchar(r + '0');
-	r = _islower('o');
-	_putchar(r + '0');
-	r = _islower(108);
+	r = _islower(n);
 	_putchar(r + '0');
 	_putchar('\n');
+}
+
+/**
+ * main - check the code for holberton school students.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	test_islower('H');
 	return (0);
 }
