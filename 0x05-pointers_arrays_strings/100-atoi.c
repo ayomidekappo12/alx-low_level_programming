2ec2836595dce8dad166723d9c8d02301a2bfab1
ayomidekappo12 <ayomidekappo12@gmail.com>
2022-12-19 17:12:48 -0800

    tasks 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,28 +7,29 @@
  */
 int _atoi(char *s)
 {
-	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
+	int i = 0;
+	int n = 0;
+	int signo = 1;
 
-	while (*(s + count) != '\0')
+	while ((s[i] < '0' || s[i] > '9') && s[i] != 0)
 	{
-		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
-			break;
-		if (*(s + count) == '-')
-			pn *= 1;
-
-		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+		if (s[i] == '-')
+			signo *= -1;
+		i++;
+	}
+	while ((s[i] >= '0' && s[i] <= '9') && s[i] != 0)
+	{
+		if (n >= 0)
 		{
-			if (size > 0)
-				m *= 10;
-			size++;
+			n = n * 10 - (s[i] - '0');
+			i++;
 		}
-		count++;
+		else
+		{
+			n = n * 10 - (s[i] - '0');
+			i++;
+		}
 	}
-
-	for (i = count - size; i < count; i++)
-	{
-		oi = oi + ((*(s + i) - 48) * m);
-		m /= 10;
-	}
-	return (oi * pn);
+	signo *= -1;
+	return (n * signo);
 }
