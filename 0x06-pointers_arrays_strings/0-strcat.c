021e8095b50ce2d6004c3504a5b5c8db2696f040
ayomidekappo12 <ayomidekappo12@gmail.com>
2022-12-23 08:46:32 -0800

    tasks 1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,32 +1,23 @@
 #include "main.h"
 
 /**
- * _strcat - concatinate the values of two integers.
- * @dest: arg a
- * @src: arg b
+ * _strcat - concatenates the string pointed to by @src, including the terminal
+ * @dest: A pointer to the string to be concatenated upon.
+ * @src: The source string to be appended to @dest.
+ * Description: 'concatenates two strings'
  *
- * Return: void.
+ * Return: A pointer to the destination string @dest.
  */
 
-char *_strcat(char *dest, char *src)
+char *strcat(char *dest, const char *src)
 {
-	int i, j;
+	int index = 0, dest_len = 0;
 
-	i = 0;
+	while (dest[index++])
+		dest_len++;
 
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
 
-	j = 0;
-
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-	dest[i] = '\0';
 	return (dest);
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,4 +1,4 @@
-#ief MAIN_H
+#ifdef MAIN_H
 #define MAIN_H
 
 #include <unistd.h>
@@ -16,6 +16,5 @@ char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
-char *leet(char *);
 
 #endif
