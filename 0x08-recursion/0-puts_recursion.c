f2272e8cc384d31d8642c9730ef653b34c1f3963
ayomidekappo12 <ayomidekappo12@gmail.com>
2023-01-04 06:14:50 -0800

    tasks 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Entry point
+ * @s: char arg
+ *
+ * Return: Alway 0 (success)
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	_putchar('\n');
+}
