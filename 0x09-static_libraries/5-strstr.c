7a8ad8e294ca846ee6031d2e9daea314845da954
ayomidekappo12 <ayomidekappo12@gmail.com>
2023-01-06 11:19:28 -0800

    tasks 1

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,33 @@
+/*
+ * File: 5-strstr.c
+ * Auth: someone who is kind
+ */
+
+#include "main.h"
+
+/**
+ * _strstr - Entry point
+ * @haystack: input
+ * @needle: input
+ * Return: Always 0 (success)
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *l = haystack;
+		char *p = needle;
+
+		while (*l == *p && *p != '\0')
+		{
+			l++;
+
+			p++;
+		}
+		if (*p == '\0')
+
+			return (haystack);
+	}
+	return (0);
+}
