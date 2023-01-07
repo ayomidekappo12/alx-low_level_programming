a37176925b203fbe52d6ebbda29b677359e79f13
ayomidekappo12 <ayomidekappo12@gmail.com>
2023-01-06 22:35:47 -0800

    tasks 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,16 +10,8 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
-
-	if (argc == 1)
-		printf("%d\n", aegc - 1);
-	else
-	{
-		for (i = 0; *argv; i++; argv++)
-			;
-		printf("%d\n", i - 1);
-	}
+(void) argv; /*Ignore argv*/
+	printf("%d\n", argc - 1);
 
 	return (0);
 }
