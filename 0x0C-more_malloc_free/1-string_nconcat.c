3ebaf5146ecf49e3cf190babe23833ce507034e3
ayomidekappo12 <ayomidekappo12@gmail.com>
2023-01-12 09:39:37 -0800

    tasks 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 #include "main.h"
 
-/*
+/**
  * string_nconcat - function that concatenates two strings.
- * @s1: This is the first input string
+ *
+ * @s1:This is the first input string
  * @s2: This is the second input string
  * @n: This is the limit to copy the second string to the first string
  *
  * Return: This is the string joined
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int a, b;
