826b7cbfb77933c831800562f72d6b5eed06b334
ayomidekappo12 <ayomidekappo12@gmail.com>
2023-01-18 12:11:32 -0800

    tasks 1

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name
+ *
+ * @name: this is the input stri ng or the input name
+ * @f: this is the input function to print the name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != 0 && name != 0)
+		f(name);
+}
