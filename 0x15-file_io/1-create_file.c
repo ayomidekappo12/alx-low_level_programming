96fa1c4fa6dcda13b9318b88e34f5f8bbaf2002e
ayomidekappo12 <ayomidekappo12@gmail.com>
2023-02-06 08:58:28 -0800

    tasks 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * create_file - create a new files
- * @filename: name of the file
- * @text_content: content of the file
- * Return: success is 1, and if failed is -1
+ * create_file - Create a new files
+ * @filename: Name of the file
+ * @text_content: Content of the file
+ * Return: Success is 1, and if failed is -1
  */
 int create_file(const char *filename, char *text_content)
 {
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		int lenWrite, len_Text;
 
-		for (len_text = 0; text_content[len_Text] != '\0'; len_Text++)
+		for (len_Text = 0; text_content[len_Text] != '\0'; len_Text++)
 			;
 		lenWrite = write(fd, text_content, (len_Text));
 		if (lenWrite == -1)
