b1e5686ec97aa063ef46b31409657993401673b0
ayomidekappo12 <ayomidekappo12@gmail.com>
2023-07-12 07:44:56 +0000

    added 0x1E-search_algorithms

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,16 +1,21 @@
-#include <stdio.h>
+#include "search_algos.h"
 
 int linear_search(int *array, size_t size, int value)
 {
-    if (array == NULL)
-        return -1;
+	if (array == NULL)
+	{
+		(return -1);
+	}
 
-    size_t i;
-    for (i = 0; i < size; i++) {
-        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-        if (array[i] == value)
-            return i;
-    }
+	for (size_t i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 
-    return -1;
+		if (array[i] == value)
+		{
+			(return i);
+		}
+	}
+
+	(return -1);
 }
--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,31 +1,40 @@
-#include <stdio.h>
+#include "search_algos.h"
+/**
+* Write a function that searches for a value in an array
+* of integers using the Linear search algorithm
+*/
 
 int binary_search(int *array, size_t size, int value)
 {
-    if (array == NULL)
-        return -1;
+	if (array == NULL)
+		(return -1);
 
-    int left = 0;
-    int right = size - 1;
+	int left = 0;
+	int right = size - 1;
+	int mid;
 
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
+	while (left <= right)
+	{
+		mid = left + (right - left) / 2;
 
-        printf("Searching in arrsy: ");
-        for (int i = left; i <= right; i++) {
-            printf("%d", array[i]);
-            if (i != right)
-                printf(", ");
-        }
-        printf("\n");
+		printf("Searching in array: ");
+		int i;
 
-        if (array[mid] == value)
-            return mid;
-        else if (array[mid] < value)
-            left = mid + 1;
-        else
-            right = mid - 1;
-    }
+		for (i = left; i <= right; i++)
+		{
+			printf("%d", array[i]);
+			if (i != right)
+				printf(", ");
+		}
+		printf("\n");
 
-    return -1;
+		if (array[mid] == value)
+			(return mid);
+		else if (array[mid] < value)
+			left = mid + 1;
+		else
+			right = mid - 1;
+	}
+
+	(return -1);
 }
