30df978da746f0c3612af24a275324ab1a1241da
ayomidekappo12 <ayomidekappo12@gmail.com>
2023-07-13 03:56:19 -0700

    added 0x1E-search_algorithms

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,8 +1,12 @@
 #include "search_algos.h"
 /**
-* Write a function that searches for a value in an array
-* of integers using the Linear search algorithm
-*/
+ * binary_search - Searches for a value in an array of integers
+ *                 using the Binary search algorithm
+ * @array: Pointer to the first element of the array to search in
+ * @size: Number of elements in the array
+ * @value: Value to search for
+ * Return: Index where value is found, or -1 if not found
+ */
 
 int binary_search(int *array, size_t size, int value)
 {
